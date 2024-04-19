7d4da803d7da4662490c0fd66ef6d82646343c2e
odobren <167509358+odobren@users.noreply.github.com>
2024-04-19 14:42:08 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,34 @@ document.getElementById("loanForm").addEventListener("submit", function(event) {
 
     var loanAmountInput = document.getElementById("loanAmount").value.trim();
     var loanTermInput = document.getElementById("loanTerm").value.trim();
+    var borrowerAgeInput = document.getElementById("borrowerAge").value.trim();
 
-    if (loanAmountInput === "" || loanTermInput === "") {
+    if (loanAmountInput === "" || loanTermInput === "" || borrowerAgeInput === "") {
         alert("Пожалуйста, заполните все поля.");
         return;
     }
 
     var loanAmount = parseFloat(loanAmountInput.replace(/\D/g, ''));
     var loanTermYears = parseFloat(loanTermInput);
+    var borrowerAge = parseFloat(borrowerAgeInput);
 
-    if (isNaN(loanAmount) || isNaN(loanTermYears) || loanAmount <= 0 || loanTermYears <= 0) {
+    if (isNaN(loanAmount) || isNaN(loanTermYears) || isNaN(borrowerAge) || loanAmount <= 0 || loanTermYears <= 0 || borrowerAge <= 0) {
         alert("Пожалуйста, введите корректные данные.");
         return;
     }
 
+    if (borrowerAge > 68) {
+        alert("Максимальный возраст заемщика 68 лет.");
+        return;
+    }
+
+    var maxLoanTerm = Math.min(15, 68 - borrowerAge); // Вычисляем максимальный срок кредита, учитывая максимальный возраст заемщика
+
+    if (loanTermYears > maxLoanTerm) {
+        alert("Максимальный срок кредита для вашего возраста " + maxLoanTerm + " лет.");
+        return;
+    }
+    
     var annualInterestRate = 18.5; // Процентная ставка 18.5%
     var monthlyInterestRate = annualInterestRate / 100 / 12;
     var loanTermMonths = loanTermYears * 12;
