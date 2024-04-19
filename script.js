fb1833bf2995574dd3af35eb9f0353d7bc836e42
odobren <167509358+odobren@users.noreply.github.com>
2024-04-19 14:59:52 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,25 +2,26 @@ document.getElementById("loanForm").addEventListener("submit", function(event) {
     event.preventDefault();
 
     var loanAmountInput = document.getElementById("loanAmount").value.trim();
-    var loanTermInput = document.getElementById("loanTerm").value.trim();
     var borrowerAgeInput = document.getElementById("borrowerAge").value.trim();
 
-    if (loanAmountInput === "" || loanTermInput === "" || borrowerAgeInput === "") {
+    if (loanAmountInput === "" || borrowerAgeInput === "") {
         alert("Пожалуйста, заполните все поля.");
         return;
     }
 
     var loanAmount = parseFloat(loanAmountInput.replace(/\D/g, ''));
-    var loanTermYears = parseFloat(loanTermInput);
     var borrowerAge = parseInt(borrowerAgeInput);
 
-    if (isNaN(loanAmount) || isNaN(loanTermYears) || isNaN(borrowerAge) || loanAmount <= 0 || loanTermYears <= 0 || borrowerAge < 18 || borrowerAge > 68) {
+    if (isNaN(loanAmount) || isNaN(borrowerAge) || loanAmount <= 0 || borrowerAge < 18 || borrowerAge > 68) {
         alert("Пожалуйста, введите корректные данные.");
         return;
     }
 
     // Определение максимального срока кредита на основе возраста заемщика
-    var maxLoanTerm = Math.min(loanTermYears, 68 - borrowerAge, 15);
+    var maxLoanTerm = Math.min(15, 68 - borrowerAge);
+
+    // Установка срока кредита в поле ввода
+    document.getElementById("loanTerm").value = maxLoanTerm;
 
     var annualInterestRate = 18.5; // Процентная ставка 18.5%
     var monthlyInterestRate = annualInterestRate / 100 / 12;
@@ -40,17 +41,12 @@ document.getElementById("loanForm").addEventListener("submit", function(event) {
     }
 });
 
-// Обработчик события изменения возраста заемщика
-document.getElementById("borrowerAge").addEventListener("input", function() {
+// Обработчик изменения возраста заемщика для автоматического обновления срока кредита
+document.getElementById("borrowerAge").addEventListener("change", function() {
     var borrowerAgeInput = document.getElementById("borrowerAge").value.trim();
-    var loanTermInput = document.getElementById("loanTerm").value.trim();
-    
-    if (borrowerAgeInput === "") {
-        return;
-    }
-    
-    var borrowerAge = parseInt(borrowerAgeInput);
-    var maxLoanTerm = Math.min(68 - borrowerAge, 15);
+    if (borrowerAgeInput === "") return;
 
+    var borrowerAge = parseInt(borrowerAgeInput);
+    var maxLoanTerm = Math.min(15, 68 - borrowerAge);
     document.getElementById("loanTerm").value = maxLoanTerm;
 });
