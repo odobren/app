f011597d1984f6a0826b0afc093d6faad4820872
odobren <167509358+odobren@users.noreply.github.com>
2024-04-19 13:49:04 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-function formatCurrency(input) {
-    var value = input.value.replace(/\D/g, '');
-    var formattedValue = new Intl.NumberFormat('ru-RU').format(value);
-    input.value = formattedValue;
+
 
 document.getElementById("loanForm2").addEventListener("submit", function(event) {
     event.preventDefault();
